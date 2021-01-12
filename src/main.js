d57efe370db6db98e415a8c9986805bdfe4d276d
railsxxx <59250712+railsxxx@users.noreply.github.com>
2021-01-12 09:30:10 +0100

    locale label rules, locale json

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import "./assets/reset.css";
 
 import { focusMQ } from "./assets/lib.js";
-import { deutsch } from "./assets/locales/deutsch.js";
+import locale from "./assets/locales/deutsch.json";
 
 const app = createApp(App);
 
 app.config.globalProperties.gClipboard = "clip";
 app.config.globalProperties.gFocusMQref = ref({});
 app.config.globalProperties.gFocusMQobj = focusMQ();
-app.config.globalProperties.gLocale = deutsch;
+app.config.globalProperties.gLocale = locale;
 
 app.mount("#app");
