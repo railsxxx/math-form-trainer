d50a3435eb67b2fea9a74635c9202d36eef96963
railsxxx <59250712+railsxxx@users.noreply.github.com>
2021-02-06 10:04:18 +0100

    fix update RuleEditor

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/reset.css";
 
-// import { focusMQ } from "./assets/lib.js";
 import locale from "./assets/locales/deutsch.json";
 
-// const app = createApp(App);
 const app = createApp(App).use(router);
 
 app.config.globalProperties.gClipboard = "clip";
 app.config.globalProperties.gFocusMQref = ref({});
-// app.config.globalProperties.gFocusMQobj = focusMQ();
-// app.config.globalProperties.gForceRerender = 0;
 app.config.globalProperties.gLocale = locale;
 
 app.mount("#app");
